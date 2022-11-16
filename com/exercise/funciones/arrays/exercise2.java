e5755481393c6121495b23e714237385abc4c5fa
Yohan Estiven Daza Rojas <yohangr4@gmail.com>
2022-11-16 16:17:07 -0500

    arrays

--- com/exercise/funciones/arrays/exercise2.java
+++ com/exercise/funciones/arrays/exercise2.java
@@ -1,4 +1,28 @@
 package com.exercise.funciones.arrays;
 
+import java.util.Scanner;
+
 public class exercise2 {
+    public static void main(String[] args) {
+
+        Scanner lectura= new Scanner (System.in);
+        System.out.println("Ingrese el número de columnas : ");
+        int columnas = lectura.nextInt();
+
+        int matriz[][]=new int[5][columnas];
+
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[0].length;j++){
+                matriz[i][j]=(generaNumAleatorio(0,9));
+                System.out.print(matriz[i][j]+" ");
+
+            }
+            System.out.println("");
+        }
+    }
+    public static int generaNumAleatorio(int minimo,int maximo){
+
+        return (int)Math.floor(Math.random()*(minimo-(maximo+1))+(maximo+1));
+
+    }
 }
